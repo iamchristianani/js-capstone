74c01395507a7332a49c5d1aa8a32a6a22dc9e6c
Fahim-Khan-P <fahim.tanvir2021@gmail.com>
2023-01-06 01:54:35 +0430

    Adds tests for comments counter

---
+++ src/__mocks__/commentsData.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    user_id: 1,
+    name: 'name',
+    comment: 'comment',
+  },
+  {
+    user_id: 2,
+    name: 'name',
+    comment: 'comment',
+  },
+  {
+    user_id: 3,
+    name: 'name',
+    comment: 'comment',
+  },
+  {
+    user_id: 4,
+    name: 'name',
+    comment: 'comment',
+  },
+];
